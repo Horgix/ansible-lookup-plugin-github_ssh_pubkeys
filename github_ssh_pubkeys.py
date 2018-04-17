428b55b3680511777fcae2289c126ef8393ddbde
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2018-04-17 11:20:36 +0200

    Clean up and add error handling

--- github_ssh_pubkeys.py
+++ github_ssh_pubkeys.py
@@ -3,10 +3,10 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible.errors import AnsibleError
-# noinspection PyProtectedMember
-from ansible.module_utils._text import to_text
-from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
 from ansible.plugins.lookup import LookupBase
+
+from github import Github
+from os import getenv
 
 try:
     from __main__ import display
@@ -15,40 +15,18 @@ except ImportError:
 
     display = Display()
 
-from github import Github
-
 
 class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
-        api_key = kwargs.get('api_key', True)
-        login = kwargs.get('login', True)
+        # Get parameters either from the task parameters or environment variables
+        api_key = kwargs.get('api_key', getenv('GITHUB_API_KEY'))
+        login = kwargs.get('login')
+
         g = Github(api_key)
 
-        ret = []
-        for key in g.get_user(login).get_keys():
-            ret.append(key.key)
+        try:
+            keys = [key.key for key in g.get_user(login).get_keys()]
             # ret.append({attr: getattr(key, '_' + attr).value  for attr in ['id', 'key', 'title', 'url', 'verified']})
-
-        # validate_certs = kwargs.get('validate_certs', True)
-        # split_lines = kwargs.get('split_lines', True)
-        # use_proxy = kwargs.get('use_proxy', True)
-
-        # for term in terms:
-        #    display.vvvv("url lookup connecting to %s" % term)
-        #    try:
-        #        response = open_url(term, validate_certs=validate_certs, use_proxy=use_proxy)
-        #    except HTTPError as e:
-        #        raise AnsibleError("Received HTTP error for %s : %s" % (term, str(e)))
-        #    except URLError as e:
-        #        raise AnsibleError("Failed lookup url for %s : %s" % (term, str(e)))
-        #    except SSLValidationError as e:
-        #        raise AnsibleError("Error validating the server's certificate for %s: %s" % (term, str(e)))
-        #    except ConnectionError as e:
-        #        raise AnsibleError("Error connecting to %s: %s" % (term, str(e)))
-
-        #    if split_lines:
-        #        for line in response.read().splitlines():
-        #            ret.append(to_text(line))
-        #    else:
-        #        ret.append(to_text(response.read()))
-        return ret
+        except Exception as e:
+            raise AnsibleError("Failed to get User's keys: {error}".format(error=str(e)))
+        return keys
